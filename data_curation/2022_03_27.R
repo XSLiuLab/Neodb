02f3b01763a1f78f89750c2c07d97e842159d297
wt12318 <1650464505@qq.com>
2022-10-31 21:16:30 +0800

    first commit

--- data_curation/2022_03_27.R
+++
@@ -1,8 +0,0 @@
-install.packages("easyPubMed")
-devtools::install_github("dami82/easyPubMed")
-library(easyPubMed)
-new_query <- 'neoantigen' 
-out.A <- batch_pubmed_download(pubmed_query_string = new_query, 
-                               format = "txt", 
-                               batch_size = 20,dest_dir = "data/",
-                               dest_file_prefix = "easyPM_example")
